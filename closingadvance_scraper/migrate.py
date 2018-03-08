4901931bde0f21cb02a4c38ff53e9f62497dcbd2
Akaki Kharchenko <akaki.kharchenko.ako@gmail.com>
2018-03-08 21:43:29 +0400

    Fix scrapers  - BrokerMobile2, BrokerMobile3, BrokerMobile4, forSale and recentlySold

--- closingadvance_scraper/migrate.py
+++ closingadvance_scraper/migrate.py
@@ -4,13 +4,53 @@ from closingadvance_scraper.database import db
 
 migrator = MySQLMigrator(db)
 
-Fax = CharField(null=True, unique=True)
+teamUrl = CharField(null=True)
+location = CharField(null=True)
+brokerMobile2 = CharField(null=True)
+brokerMobile3 = CharField(null=True)
+brokerMobile4 = CharField(null=True)
 
 migrate(
-    migrator.add_column('realtor_agents', 'fax', Fax),
+    migrator.add_column('realtor_brokers', 'teamUrl', teamUrl),
 )
 
 migrate(
-    migrator.add_column('realtor_brokers', 'fax', Fax),
+    migrator.add_column('realtor_brokers', 'location', location),
 )
 
+migrate(
+    migrator.add_column('realtor_brokers', 'brokerMobile2', brokerMobile2),
+)
+
+migrate(
+    migrator.add_column('realtor_brokers', 'brokerMobile3', brokerMobile3),
+)
+
+migrate(
+    migrator.add_column('realtor_brokers', 'brokerMobile4', brokerMobile4),
+)
+
+
+forSale = IntegerField(null=True)
+recentlySold = IntegerField(null=True)
+brokerUrl = CharField(null=True)
+
+migrate(
+    migrator.add_column('realtor_agents', 'teamUrl', teamUrl),
+)
+
+migrate(
+    migrator.add_column('realtor_agents', 'brokerUrl', brokerUrl),
+)
+
+migrate(
+    migrator.add_column('realtor_agents', 'location', location),
+)
+
+migrate(
+    migrator.add_column('realtor_agents', 'forSale', forSale),
+)
+
+migrate(
+    migrator.add_column('realtor_agents', 'recentlySold', recentlySold),
+)
