bb4ece02c2fed8784afa5343013266d90fe56bb6
Akaki Kharchenko <akakik@avanan.com>
2018-04-02 15:15:52 +0400

    Implement realtor_listing_qualified_for_julien_spider

--- closingadvance_scraper/spiders/realtor_listing_qualified_for_julien.py
+++ closingadvance_scraper/spiders/realtor_listing_qualified_for_julien.py
@@ -24,7 +24,7 @@ class RealtorListingForJulienSpider(scrapy.Spider):
 
     def start_requests(self):
         self.listingStatus = [user._data['status'] for user in ListingStatus.select()]
-        input_file = csv.DictReader(open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/input/realtor_agents.csv"), delimiter=";")
+        input_file = csv.DictReader(open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/filtered agent list.csv"), delimiter=";")
 
         for row in input_file:
             yield scrapy.Request(row["originUrl"], callback=self.parse, meta={'agent_id': row["id"], 'brokers_list': row["brokers_list"]})
