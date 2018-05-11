d8412abb8fa510c7141283bae2c484da9ec5bf23
Akaki Kharchenko <akakik@avanan.com>
2018-05-11 08:01:45 +0400

    change crawlera api key

--- closingadvance_scraper/scripts/download_realtor_listing.py
+++ closingadvance_scraper/scripts/download_realtor_listing.py
@@ -24,6 +24,8 @@ with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/outpu
 shuffle(listing_urls_list)
 
 for listing_url in listing_urls_list:
+    print listing_url
+
     try:
         payload = {"headers": {}, "method": "GET", "url": listing_url}
         headers = {}
@@ -34,14 +36,12 @@ for listing_url in listing_urls_list:
 
         if html_content:
             html_content = html_content.encode('utf-8')
-            try:
-                listing_id = re.findall(re.compile(r'"property_id":(.*?),', flags=re.DOTALL), html_content)[0].strip()
-                if listing_id:
-                    with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/listing_pages/{0}.html".format(listing_id), "w") as listing_file:
-                        listing_file.write(html_content)
-            except Exception as e:
-                with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/failure_list.csv",
-                          "a") as output_file:
-                    output_file.write(listing_url + "\n")
+            listing_id = re.findall(re.compile(r'"property_id":(.*?),', flags=re.DOTALL), html_content)[0].strip()
+            if listing_id:
+                with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/listing_pages/{0}.html".format(listing_id), "w") as listing_file:
+                    listing_file.write(html_content)
     except Exception as e:
+        with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/failure_list.csv",
+                  "a") as output_file:
+            output_file.write(listing_url + "\n")
         print(e)
