12509f29017b2f8981ca123181c39ea993d6a786
Akaki Kharchenko <akakik@avanan.com>
2018-05-11 13:27:08 +0400

    change crawlera api key

--- closingadvance_scraper/spiders/realtor_listing_page_downloader.py
+++ closingadvance_scraper/spiders/realtor_listing_page_downloader.py
@@ -49,10 +49,12 @@ class RealtorListingFromQualifiedUrlsSpider(scrapy.Spider):
 
         if response.status == 200:
             try:
-                listing_id = re.findall(re.compile(r'"property_id":(.*?),', flags=re.DOTALL), response.text)[0].strip()
+                response_json = json.loads(response.text)
+                html_content = response_json["response"]["body"]
+                listing_id = re.findall(re.compile(r'"property_id":(.*?),', flags=re.DOTALL), html_content)[0].strip()
                 if listing_id:
                     with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/listing_pages/{0}.html".format(listing_id), "w") as listing_file:
-                        listing_file.write(response.text)
+                        listing_file.write(html_content)
             except Exception as e:
                 with open(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/failure_list.csv",
                           "a") as output_file:
