8158baeae72de3b54bb317b74536ed05e79351db
Akaki Kharchenko <akakik@avanan.com>
2018-07-22 04:51:55 +0400

    change crawlera api key

--- closingadvance_scraper/scripts/download_realtor_agent_profile_from_listing.py
+++ closingadvance_scraper/scripts/download_realtor_agent_profile_from_listing.py
@@ -8,11 +8,12 @@ import os
 import re
 import requests
 import time
+import random
 import json
 from lxml import etree
 from fake_useragent import UserAgent
 
-luminati_port = '22999'
+luminati_port = '33999'
 realtor_home_url = "https://www.realtor.com"
 
 if not os.path.exists(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/agent_profile_pages"):
@@ -22,68 +23,75 @@ new_listing_urls_list = []
 ua = UserAgent()
 continuous_failure = 0
 
+listing_pages_path_list = []
+
 for root, subdirs, files in os.walk(os.path.dirname(os.path.realpath(__file__)) + "/../external_data/output/listing_pages"):
     for filename in files:
         file_path = os.path.join(root, filename)
-        parser = etree.HTMLParser()
-        page_tree = etree.parse(file_path, parser)
-        agent_profile_link = page_tree.xpath("//a[@data-omtag='ldp:listingProvider:agentProfile']/@href")
+        listing_pages_path_list.append(file_path)
 
-        if not agent_profile_link:
-            continue
+random.shuffle(listing_pages_path_list)
 
-        agent_profile_link = realtor_home_url + agent_profile_link[0]
-        agent_profile_page_name = agent_profile_link[agent_profile_link.rfind('/') + 1:]
+for file_path in listing_pages_path_list:
+    parser = etree.HTMLParser()
+    page_tree = etree.parse(file_path, parser)
+    agent_profile_link = page_tree.xpath("//a[@data-omtag='ldp:listingProvider:agentProfile']/@href")
 
-        if os.path.exists(os.path.dirname(os.path.realpath(__file__)) +
-                          "/../external_data/output/agent_profile_pages/{0}.html".format(agent_profile_page_name)):
-            continue
+    if not agent_profile_link:
+        continue
 
-        try:
-            retry_limit = 10
+    agent_profile_link = realtor_home_url + agent_profile_link[0]
+    agent_profile_page_name = agent_profile_link[agent_profile_link.rfind('/') + 1:]
 
-            while retry_limit > 0:
-                try:
-                    headers = {}
-                    headers['User-Agent'] = ua.chrome
-                    headers['Content-Type'] = "application/json"
-                    payload = {"headers": headers, "method": "GET", "url": agent_profile_link}
-                    response_json = json.loads(requests.post("http://127.0.0.1:{0}/api/test/24000".format(luminati_port), data=payload).text)
+    if os.path.exists(os.path.dirname(os.path.realpath(__file__)) +
+                      "/../external_data/output/agent_profile_pages/{0}.html".format(agent_profile_page_name)):
+        continue
+
+    try:
+        retry_limit = 10
+
+        while retry_limit > 0:
+            try:
+                headers = {}
+                headers['User-Agent'] = ua.chrome
+                headers['Content-Type'] = "application/json"
+                payload = {"headers": headers, "method": "GET", "url": agent_profile_link}
+                response_json = json.loads(requests.post("http://127.0.0.1:{0}/api/test/24000".format(luminati_port), data=payload).text)
+                html_content = response_json["response"]["body"]
+                html_content = html_content.encode('utf-8')
+
+                if html_content.startswith("<html><body>You are being "):
+                    agent_profile_link = re.findall(re.compile(r'<a href="(.*?)">', flags=re.DOTALL), html_content)[
+                        0].strip()
+
+                    payload["url"] = agent_profile_link
+                    response_json = json.loads(requests.post("http://127.0.0.1:{0}/api/test/24000".format(luminati_port),
+                                                             data=payload).text)
                     html_content = response_json["response"]["body"]
                     html_content = html_content.encode('utf-8')
 
-                    if html_content.startswith("<html><body>You are being "):
-                        agent_profile_link = re.findall(re.compile(r'<a href="(.*?)">', flags=re.DOTALL), html_content)[
-                            0].strip()
-
-                        payload["url"] = agent_profile_link
-                        response_json = json.loads(requests.post("http://127.0.0.1:{0}/api/test/24000".format(luminati_port),
-                                                                 data=payload).text)
-                        html_content = response_json["response"]["body"]
-                        html_content = html_content.encode('utf-8')
-
-                    break
-                except Exception as e:
-                    time.sleep(6)
-                    retry_limit -= 1
-
-            if retry_limit == 0:
-                raise Exception()
-
-            if html_content:
-                mls_id = re.findall(re.compile(r'MLSID: "(.*?)",', flags=re.DOTALL), html_content)[0].strip()
-                if mls_id:
-                    with open(os.path.dirname(os.path.realpath(__file__)) +
-                              "/../external_data/output/agent_profile_pages/{0}.html".format(
-                                  agent_profile_page_name), "w") as agent_profile_file:
-                        agent_profile_file.write(html_content)
-                        agent_profile_file.close()
-                    continuous_failure = 0
-                    print mls_id + "  -  " + agent_profile_link
-        except Exception as e:
-            print(e)
-            continuous_failure += 1
-            if continuous_failure > 10:
-                print("Proxy doesn't work properly!!!")
                 break
+            except Exception as e:
+                time.sleep(6)
+                retry_limit -= 1
+
+        if retry_limit == 0:
+            raise Exception()
+
+        if html_content:
+            mls_id = re.findall(re.compile(r'MLSID: "(.*?)",', flags=re.DOTALL), html_content)[0].strip()
+            if mls_id:
+                with open(os.path.dirname(os.path.realpath(__file__)) +
+                          "/../external_data/output/agent_profile_pages/{0}.html".format(
+                              agent_profile_page_name), "w") as agent_profile_file:
+                    agent_profile_file.write(html_content)
+                    agent_profile_file.close()
+                continuous_failure = 0
+                print mls_id + "  -  " + agent_profile_link
+    except Exception as e:
+        print(e)
+        continuous_failure += 1
+        if continuous_failure > 10:
+            print("Proxy doesn't work properly!!!")
+            break
 
