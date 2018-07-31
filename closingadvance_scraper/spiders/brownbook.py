17eac0fea5d2c35169d3c8a0c354ef7ccffb093b
Akaki Kharchenko <akakik@avanan.com>
2018-07-31 10:38:38 +0400

    change crawlera api key

--- closingadvance_scraper/spiders/brownbook.py
+++ closingadvance_scraper/spiders/brownbook.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
-import random
+from random import shuffle
 from closingadvance_scraper.items import BrownBookItem
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
@@ -17,7 +17,10 @@ class BrownBookSpider(scrapy.Spider):
         yield scrapy.Request(self.start_url, callback=self.parse_categories_list_page)
 
     def parse_categories_list_page(self, response):
-        for category_link in response.xpath('//div[@id="promo_full"]//table//td/a/@href').extract():
+        categories_link_list = response.xpath('//div[@id="promo_full"]//table//td/a/@href').extract()
+        shuffle(categories_link_list)
+
+        for category_link in categories_link_list:
             yield response.follow(category_link,
                                   callback=self.parse_category_page,
                                   cookies={'setcountry': 'us'})
